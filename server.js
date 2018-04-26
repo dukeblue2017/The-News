1e0867147af6fa1533772db1bcceb7874947a245
Carter Duncan <dukeblue2017@gmail.com>
2018-04-26 12:15:50 -0500

    Add knex, dotenv, further styling, submissions page

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const knex = require('knex');
+const dotenv = require('dotenv').config({
+  path: 'env.env'
+});
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : process.env.host,
+    user : process.env.user,
+    password : process.env.password,
+    database : process.env.database,
+  }
+});
 
-
-app.use(express.static(path.join(__dirname, '')))
+app.use(express.static(path.join(__dirname, '')));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
+
+knex.select().table('articles').then( (success, err) => {
+  console.log('success', success)
+});
