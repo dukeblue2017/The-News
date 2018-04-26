d759d606c7bd29a8ff0e84b2ec0fa9f28c660af7
Carter Duncan <dukeblue2017@gmail.com>
2018-04-26 14:20:11 -0500

    Build out the submissions page and functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config({
   path: 'env.env'
 });
@@ -14,10 +15,26 @@ const knex = require('knex')({
   }
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '')));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
 
-knex.select().table('articles').then( (success, err) => {
-  console.log('success', success)
-});
+app.get('/submissions', (req, res) => {
+})
+
+app.post('/submissions', (req, res) => {
+  console.log(req.body);
+  knex('articles').insert({
+    originalTitle: req.body.originalTitle,
+    ourTitle: req.body.ourTitle,
+    URL: req.body.URL,
+    rank: req.body.rank,
+    date: req.body.date,
+  }).then( (success) => {
+    res.send('Successfully submitted.');
+  }).catch( (err) => {
+    res.send('Error in submission.')
+  })
+})
