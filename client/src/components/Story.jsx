cfd2a208bf2bd43fe83bdec8e0e0777ebcf926e3
Carter Duncan <dukeblue2017@gmail.com>
2018-05-08 13:19:19 -0500

    Lint Story.jsx & server.js & Story.jsx --> function

--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -1,27 +1,21 @@
-import React, { Component} from "react";
+import React from 'react';
 
-class Story extends Component{
-  constructor(props) {
-    super(props)
-  }
-
-  render(){
-    return(
-      <div className="story">
-        <div className="story-title">
-          <a href={this.props.URL}>{this.props.title}</a>
-        </div>
-        <div className="story-text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+function Story(props) {
+  return (
+    <div className="story">
+      <div className="story-title">
+        <a href={props.URL}>{props.title}</a>
       </div>
-    );
-  }
+      <div className="story-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
+  );
 }
 
 export default Story;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv').config({
-  path: 'env.env'
+const dotenv = require('dotenv').config({ //eslint-disable-line
+  path: 'env.env',
 });
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host : process.env.host,
-    user : process.env.user,
-    password : process.env.password,
-    database : process.env.database,
-  }
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+  },
 });
+
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,7 +27,7 @@ app.listen(3000, () => console.log('Listening on port 3000'));
 
 app.get('/submissions', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/submissions.html'));
-})
+});
 
 app.post('/submissions', (req, res) => {
   knex('articles').insert({
@@ -35,14 +36,13 @@ app.post('/submissions', (req, res) => {
     URL: req.body.URL,
     rank: req.body.rank,
     date: req.body.date,
-    column: req.body.column
-  }).then( (success) => {
+    column: req.body.column,
+  }).then(() => {
     res.send('Successfully submitted.');
-  }).catch( (err) => {
-    console.log('err in post to /submissions', err)
+  }).catch((err) => {
     res.send('Error in submission.');
-  })
-})
+  });
+});
 
 app.get('/stories', (req, res) => {
   knex.raw('SELECT originalTitle, `column`, URL FROM articles WHERE date = CURDATE()')
@@ -51,5 +51,5 @@ app.get('/stories', (req, res) => {
     })
     .catch((DBres) => {
       res.send(DBres);
-    })
-})
+    });
+});
