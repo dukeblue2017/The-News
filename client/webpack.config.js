9297c5afc6e9e8084ce5a084e5adad7463799f50
Carter Duncan <dukeblue2017@gmail.com>
2018-05-08 13:01:08 -0500

    Lint and rename index.js --> index.jsx
    Changed autocrlf to false in repo config
    so that I could commit index.jsx

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-  entry: path.join(__dirname, "./src/index.js"),
+  entry: path.join(__dirname, "./src/index.jsx"),
   module: {
     rules: [
       {
